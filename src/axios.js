2b602811d7d64197622d892c303020bd69c33f72
Emmanuel David <81506072+Emman1320@users.noreply.github.com>
2021-11-03 08:54:44 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://us-central1-mail-mate.cloudfunctions.net/api", //THE API (cloud function) URL
+  baseURL: API_URL, //THE API (cloud function) URL
 });
 
 export default instance;
