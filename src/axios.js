c40fc803255619c0ea17003b644a668a272c3daf
Emmanuel David <81506072+Emman1320@users.noreply.github.com>
2022-02-07 16:47:35 +0530

    Delete axios.js

--- src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://us-central1-mail-mate.cloudfunctions.net/api",
-});
-
-export default instance;
-// baseURL: "http://localhost:5001/mail-mate/us-central1/api", //THE API (cloud function) URL
-
